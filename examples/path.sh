16e5eebf82bdda14a01a38795f0c267a9cf03e1b
yhyan <yhyan@geek.(none)>
2012-09-03 15:00:22 +0800

    find $0 path.

---
+++ examples/path.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+THIS_FILE=`readlink -f $0`
+SCRIPTS_DIR=`dirname $THIS_FILE`
+TOP_DIR=`dirname $SCRIPTS_DIR`
+
+echo $0
+echo $THIS_FILE
+echo $SCRIPTS_DIR
+echo $TOP_DIR
+
+
