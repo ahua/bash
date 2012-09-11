4ce967c0934e6abf27231b22759bf87b68518dc1
yhyan <yhyan@geek.(none)>
2012-09-12 02:09:07 +0800

    a

---
+++ examples/coding.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# $ file -bi xx.txt
+# ISO-8859 text
+
+iconv -f ISO-8859 -t utf8 inputfile
+
+
---
+++ examples/delblank.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+find -name "* *" -type f | rename 's/ //g'
+
