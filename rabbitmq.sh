644de7d4f6861104080111cee7dd9971f58880e3
yhyan <yhyan@gengo>
2013-03-19 01:13:47 +0800

    add ra

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#http://www.rabbitmq.com/install-debian.html
+
 echo "deb http://www.rabbitmq.com/debian/ testing main" | sudo tee -a /etc/apt/sources.list
 cd /var/tmp/
 wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
