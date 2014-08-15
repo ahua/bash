f47a31b55685d50d7007a4d38794a7901ec506c0
Yahua Yan <yanyh@tupo.com>
2014-08-16 00:41:07 +0800

    .

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -4,6 +4,8 @@ sudo apt-get install -y ipython python-pip
 sudo apt-get install -y build-essential tree terminator emacs23-nox vim
 sudo apt-get install python-dev
 sudo pip install tornado pymongo pyredis SQLAlchemy
-
+sudo apt-get install libmysqlclient-dev
+sudo pip install thrift
+sudo pip install mysql-connector-python MySQL-python
 # tupo
 sudo pip install pyes
