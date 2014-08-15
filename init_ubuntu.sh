267f038d36b6801aabee9c367024ae349ba0ff5f
Yahua Yan <yanyh@tupo.com>
2014-08-16 00:28:37 +0800

    .

---
+++ init_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+sudo apt-get install -y ipython python-pip 
+sudo apt-get install -y build-essential tree terminator emacs23-nox vim
+sudo apt-get install python-dev
+sudo pip install tornado pymongo pyredis SQLAlchemy
+
+# tupo
+sudo pip install pyes
